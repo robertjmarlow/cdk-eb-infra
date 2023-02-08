cddef876386ab6c25fb0a4053f9bc166fd0672c5
rm025953 <robert.marlow@cerner.com>
2023-02-08 14:38:53 -0600

    Copypasting stuff from module 2

--- lib/cdk-eb-infra-stack.ts
+++ lib/cdk-eb-infra-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 // import * as sqs from '@aws-cdk/aws-sqs';
 import s3assets = require('@aws-cdk/aws-s3-assets');
+import elasticbeanstalk = require('@aws-cdk/aws-elasticbeanstalk');
+import iam = require('@aws-cdk/aws-iam');
 
 export class CdkEbInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,6 +13,77 @@ export class CdkEbInfraStack extends cdk.Stack {
     const webAppZipArchive = new s3assets.Asset(this, 'WebAppZip', {
       path: `${__dirname}/../app.zip`,
     });
+
+    // i'm so lost rn: https://aws.amazon.com/getting-started/guides/deploy-webapp-elb/module-two/
+
+    // Create a ElasticBeanStalk app.
+    const appName = 'MyWebApp';
+    const app = new elasticbeanstalk.CfnApplication(this, 'Application', {
+        applicationName: appName,
+    });
+
+    // Create an app version from the S3 asset defined earlier
+    const appVersionProps = new elasticbeanstalk.CfnApplicationVersion(this, 'AppVersion', {
+      applicationName: appName,
+      sourceBundle: {
+          s3Bucket: webAppZipArchive.s3BucketName,
+          s3Key: webAppZipArchive.s3ObjectKey,
+      },
+    });
+
+    // Make sure that Elastic Beanstalk app exists before creating an app version
+    appVersionProps.addDependsOn(app);
+
+    // Create role and instance profile
+    const myRole = new iam.Role(this, `${appName}-aws-elasticbeanstalk-ec2-role`, {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+    });
+
+    const managedPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName('AWSElasticBeanstalkWebTier')
+    myRole.addManagedPolicy(managedPolicy);
+
+    const myProfileName = `${appName}-InstanceProfile`
+
+    const instanceProfile = new iam.CfnInstanceProfile(this, myProfileName, {
+      instanceProfileName: myProfileName,
+      roles: [
+          myRole.roleName
+      ]
+    });
+
+    // Example of some options which can be configured
+    const optionSettingProperties: elasticbeanstalk.CfnEnvironment.OptionSettingProperty[] = [
+      {
+          namespace: 'aws:autoscaling:launchconfiguration',
+          optionName: 'IamInstanceProfile',
+          value: myProfileName,
+      },
+      {
+          namespace: 'aws:autoscaling:asg',
+          optionName: 'MinSize',
+          value: '1',
+      },
+      {
+          namespace: 'aws:autoscaling:asg',
+          optionName: 'MaxSize',
+          value: '1',
+      },
+      {
+          namespace: 'aws:ec2:instances',
+          optionName: 'InstanceTypes',
+          value: 't2.micro',
+      },
+    ];
+
+    // Create an Elastic Beanstalk environment to run the application
+    const elbEnv = new elasticbeanstalk.CfnEnvironment(this, 'Environment', {
+      environmentName: 'MyWebAppEnvironment',
+      applicationName: app.applicationName || appName,
+      solutionStackName: '64bit Amazon Linux 2 v5.6.1 running Node.js 14',
+      optionSettings: optionSettingProperties,
+      versionLabel: appVersionProps.ref,
+    });
+
     // example resource
     // const queue = new sqs.Queue(this, 'CdkEbInfraQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
